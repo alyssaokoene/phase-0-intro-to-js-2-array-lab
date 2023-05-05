7409035cf05ba36a0d4572071cb2af9cd1cf6604
Alyssa Okoene <alyssaokoenebusiness@gmail.com>
2023-05-04 22:08:31 -0500

    complete javascript arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+const allCats = [...cats, "Broom"];
+function appendCat(name) {
+   return allCats
+}
+
+const allCats2 = ["Arnold", ...cats];
+function prependCat(name) {
+    return allCats2
+}
+
+const newCats = cats.slice(0, -1);
+function removeLastCat() {
+    return newCats;
+}
+
+const newCats2 = cats.slice(1);
+function removeFirstCat() {
+    return newCats2;
+}
